f2d78040471a9049a0d54760e45ec906787aa3d7
JhonOlivares26 <89679333+JhonOlivares26@users.noreply.github.com>
2023-03-19 16:06:49 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,24 @@ public class Main {
     public static void main(String[] args) {
 
         List<Colegio> colegios = new ArrayList<>();
-        colegios.add(new Colegio("I.E Jose", 2176, 100));
-        colegios.add(new Colegio("I.E Divino", 2135, 500));
-        colegios.add(new Colegio("I.E La presentacion", 1456, 450));
+        colegios.add(new Colegio("I.E Jose", "2176", 100));
+        colegios.add(new Colegio("I.E Divino", "2135", 500));
+        colegios.add(new Colegio("I.E La presentacion", "1456", 450));
 
         List<Proveedor> proveedores = new ArrayList<>();
         proveedores.add(new Proveedor("Colanta", "32424323"));
         proveedores.add(new Proveedor("ALpina", "324243223"));
         proveedores.add(new Proveedor("Fruber", "325444323"));
 
+
         while (true) {
             Scanner entrada = new Scanner(System.in);
             System.out.println("\n");
             System.out.println("Ingrese que quieres hacer");
             System.out.println("1. Registar nuevo colegio: ");
-            System.out.println("2. Registar proveedor");
-            System.out.println("3. ");
-            System.out.println("4. ");
+            System.out.println("2. Registar proovedor");
+            System.out.println("3. Ver informacion de un colegio: ");
+            System.out.println("4. Ver informacion de un proveedor: ");
             System.out.println("5. ");
             int opcion = entrada.nextInt();
 
@@ -38,11 +39,11 @@ public class Main {
             }
             if (opcion == 3) {
 
-
+                Colegio.Ver_Informacion_Col(colegios);
             }
             if (opcion == 4) {
 
-
+                Proveedor.Ver_Informacion_Prov(proveedores);
             }
             if (opcion == 5) {
 
